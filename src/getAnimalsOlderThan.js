f19a0729093295c84c8ccf5b434c42a54e478364
Alex Horman de Medeiros Correia <alexhorman1990@gmail.com>
2022-01-27 01:00:58 -0300

    requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,10 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  const funcao = species.find((element) => animal === element.name);
+  return funcao.residents.every((elementTwo) => age <= elementTwo.age);
 }
 
 module.exports = getAnimalsOlderThan;
