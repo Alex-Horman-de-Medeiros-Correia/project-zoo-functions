8e22dd3d1df26ed28356ea366d3cb5b9418e0b48
Alex Horman de Medeiros Correia <alexhorman1990@gmail.com>
2022-01-27 02:50:32 -0300

    requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,23 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
 function isManager(id) {
   // seu código aqui
+  return employees.some((element) => element.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
   // seu código aqui
+  if (!isManager(managerId)) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  const abrigo = [];
+  employees.forEach((element) => {
+    if (element.managers.includes(managerId)) {
+      abrigo.push(`${element.firstName} ${element.lastName}`);
+    }
+  });
+  return abrigo;
 }
 
 module.exports = { isManager, getRelatedEmployees };
