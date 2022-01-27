a3bbbfd5e6cac8b5414e137acb6d52109ce4d63b
Alex Horman de Medeiros Correia <alexhorman1990@gmail.com>
2022-01-27 20:22:03 -0300

    requisito 9... Sem forças ja...

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
 
+const { employees, species } = data;
 function getOldestFromFirstSpecies(id) {
   // seu código aqui
+  const empregado = employees.find((element) => element.id === id).responsibleFor[0];
+  const animal = species.find((elementTwo) => elementTwo.id === empregado).residents;
+  animal.sort((um, dois) => dois.age - um.age);
+
+  /* return [...animal]; */
+  return Object.values(animal[0]);
 }
 
 module.exports = getOldestFromFirstSpecies;
