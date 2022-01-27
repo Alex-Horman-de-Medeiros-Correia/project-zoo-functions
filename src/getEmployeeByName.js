7ac689a78bb766c216ab4dd2d8f2996166fd2136
Alex Horman de Medeiros Correia <alexhorman1990@gmail.com>
2022-01-27 01:46:11 -0300

    requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  if (!employeeName) {
+    return {};
+  }
+  return employees.find((element) => employeeName === element.firstName
+  || employeeName === element.lastName);
 }
+
+console.log(getEmployeeByName('Emery'));
 
 module.exports = getEmployeeByName;
