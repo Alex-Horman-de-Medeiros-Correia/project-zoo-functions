39424724b74a1a7b7c909f15cc747c16ccdf36d8
Alex Horman de Medeiros Correia <alexhorman1990@gmail.com>
2022-01-27 05:05:27 -0300

    requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,21 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
 function countAnimals(animal) {
   // seu código aqui
+  if (!animal) {
+    const abrigo = {};
+    species.map((element) => {
+      abrigo[element.name] = element.residents.length;
+      return abrigo;
+    });
+    return abrigo;
+  }
+
+  const { residents } = species.find((elementOne) => elementOne.name === animal.specie);
+  const contador = residents.filter((elementTwo) => elementTwo.sex === animal.sex).length;
+
+  return (animal.sex) ? contador : residents.length;
 }
 
 module.exports = countAnimals;
